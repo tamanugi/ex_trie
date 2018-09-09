5deaf048f3c10e67d75703e0a258a586b53c1799
tamanugi <tamanugi@gmail.com>
2018-09-09 21:59:12 +0900

    initial commit

---
+++ lib/ex_trie.ex
@@ -0,0 +1,18 @@
+defmodule ExTrie do
+  @moduledoc """
+  Documentation for ExTrie.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTrie.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_trie_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTrieTest do
+  use ExUnit.Case
+  doctest ExTrie
+
+  test "greets the world" do
+    assert ExTrie.hello() == :world
+  end
+end
